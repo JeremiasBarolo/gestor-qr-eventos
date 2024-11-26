4b6f8f18321323b8497efa4642204a8b5bc685fc
Tomas Tosello <tomitosello@gmail.com>
2024-11-26 09:33:16 -0300

    Moficiado Tarjetas de Eventos y Tarjestas QR

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json()) 
 app.use(cors());
-// app.use(middlewareModule.validateToken); 
+app.use(middlewareModule.validateToken); 
 
 
 // QR
--- frontend/src/app/components/eventos/eventos.component.ts
+++ frontend/src/app/components/eventos/eventos.component.ts
@@ -15,6 +15,9 @@ export class EventosComponent {
   fecha_evento:any
   nombre_evento:any
   id_evento:any
+  searchTerm: string = '';
+  eventosFiltrados: any[] = []; 
+
 
   constructor(
     private apiService: ApiService,
@@ -28,51 +31,63 @@ export class EventosComponent {
   }
 
 
-  cargarDatos(){
-    this.apiService.getEventos().subscribe( (evento) =>{
-      this.eventos = evento
-    })
+  cargarDatos() {
+    this.apiService.getEventos().subscribe((evento) => {
+      this.eventos = evento;
+      this.eventosFiltrados = [...this.eventos]; // Inicializa con todos los eventos
+    });
   }
 
-  crearEvento(){
-    let data = {nombre_evento: this.nombre_evento, fecha_evento:new Date(this.fecha_evento)}
-    if(this.action ==  'crear'){
-      this.apiService.createEventos(data).subscribe( (res)=>{
-        if(res){
-          this.eventos = []
-          this.cargarDatos()
-        }
-      })
-    }else{
-      this.apiService.updateEventos(this.id_evento,data).subscribe( (res)=>{
-        if(res){
-          this.eventos = []
-          this.cargarDatos()
-        }
-      })
+  filtrarEventos() {
+    const term = this.searchTerm.toLowerCase().trim();
+    if (!term) {
+      this.eventosFiltrados = [...this.eventos]; // Mostrar todos si no hay término
+    } else {
+      this.eventosFiltrados = this.eventos.filter((evento) =>
+        evento.nombre_evento.toLowerCase().includes(term)
+      );
     }
   }
 
+  crearEvento() {
+    let data = {
+      nombre_evento: this.nombre_evento,
+      fecha_evento: new Date(this.fecha_evento),
+    };
+    if (this.action == 'crear') {
+      this.apiService.createEventos(data).subscribe((res) => {
+        if (res) {
+          this.eventos = [];
+          this.cargarDatos();
+        }
+      });
+    } else {
+      this.apiService.updateEventos(this.id_evento, data).subscribe((res) => {
+        if (res) {
+          this.eventos = [];
+          this.cargarDatos();
+        }
+      });
+    }
+  }
 
-  editar(data:any){
-    this.nombre_evento = data.nombre_evento
-    this.id_evento = data.id
+  editar(data: any) {
+    this.nombre_evento = data.nombre_evento;
+    this.id_evento = data.id;
     const fecha = new Date(data.fecha_evento);
     this.fecha_evento = fecha.toISOString().split('T')[0];
   }
 
-
-  eliminarEvento(id:any){
-    this.apiService.deleteEventos(id).subscribe((res)=>{
-      if(res){
-        this.eventos = []
-        this.cargarDatos()
+  eliminarEvento(id: any) {
+    this.apiService.deleteEventos(id).subscribe((res) => {
+      if (res) {
+        this.eventos = [];
+        this.cargarDatos();
       }
-    })
+    });
   }
 
   verEntradas(id: number) {
     this.router.navigate([`ver-entradas`, id]);
   }
-
 }
